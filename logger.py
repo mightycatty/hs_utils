1fb8cb663fd16827d8012b28f9f7a580e227d3fc
mightycatty <Howdareyou123>
2019-12-03 00:11:03 +0800

    updata logger

--- logger.py
+++ logger.py
@@ -8,26 +8,25 @@ class MyLog(object):
     requirement: telebot
         https://github.com/eternnoir/pyTelegramBotAPI
     """
-    def __init__(self, log_file='output.log', tele_bot_token=None, chat_id=None, clean_format=True):
+    def __init__(self, log_name='output', logging_level=logging.DEBUG, clean_format=True):
         # create logger
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(log_name)
         self.logger.setLevel(logging.DEBUG)
-        self.log_file = log_file
+        self.log_file = log_name + '.log'
         ch = logging.FileHandler(self.log_file)
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(logging_level)
         if not clean_format:
             formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         else:
             formatter = logging.Formatter('%(message)s')
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
+        self.tb = None
+
+    def telegram_bot_init(self, token=None):
         # initialize telegram bot
         _default_token = '664787432:AAFkb3Q_mMXlid29fwWibIUOeaNWvtHSHpg'
-        self.token = _default_token if tele_bot_token is None else tele_bot_token
-        # bot与你的聊天的chat id，默认是你自己
-        _default_chat_id = '786535272'
-        # 如果你的网络环境不科学，请自备工具，否则会连不上服务器
-        self.chat_id = _default_chat_id if chat_id is None else tele_bot_token
+        self.token = token if token else _default_token
         self.tb = telebot.TeleBot(self.token)
 
     def debug(self, message):
@@ -47,7 +46,7 @@ class MyLog(object):
             pass
         return
 
-    def fire_message_via_bot(self, message, chat_id=None):
+    def fire_message_via_bot(self, message, chat_id=None, token=None):
         """
         向指定的chat id发送文字信息
         注意：由于采用markdown解析，字符串中不能出现markdown的语义符，否则报bad request错误
@@ -55,14 +54,16 @@ class MyLog(object):
         :param chat_id:
         :return:
         """
-        response = None
-        if chat_id is None:
-            chat_id = self.chat_id
-        if (self.token is None) or (chat_id is None):
-            self.logger.warning('token or chat_id is required!')
+        chat_id = '786535272' if chat_id is None else chat_id
+        if self.tb is None:
+            if (token is None) or (chat_id is None):
+                self.logger.warning('token or chat_id is required!')
+            else:
+                self.token = token
+                self.chat_id = chat_id
+                self.tb = telebot.TeleBot(self.token)
         else:
-            response = self.tb.send_message(chat_id, message, parse_mode='Markdown')
-        return response
+            self.tb.send_message(chat_id, message, parse_mode='Markdown')
 
 
 if __name__ == '__main__':
